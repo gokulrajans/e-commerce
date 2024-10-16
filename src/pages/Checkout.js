f094b327d8112a8e4fe987428465b4b11f5c3710
GOKULRAJ VARATHARAJAN <gokul19.v@gmail.com>
2024-10-16 09:56:54 +0530

    Publish changes are updated

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import CheckoutPage from '../components/CheckOut/CheckoutPage';
 import './Checkout.css';
 
 const Checkout = () => {
-  const [orderPlaced, setOrderPlaced] = useState(false);
+  // const [orderPlaced, setOrderPlaced] = useState(false);
 
-  const handleCheckout = () => {
-    // Simulate checkout process
-    setOrderPlaced(true);
-  };
+  // const handleCheckout = () => {
+  //   // Simulate checkout process
+  //   setOrderPlaced(true);
+  // };
 
   return (
     <div className='checkout-main-page'>
